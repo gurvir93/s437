0c09af5551355c097cd384ee4586dad92dba5779
nbrons <nickbrons@msn.com>
2017-02-13 17:54:11 -0700

    test cases for Contains and Equals

--- src/org/jfree/data/test/RangeTest.java
+++ src/org/jfree/data/test/RangeTest.java
@@ -23,6 +23,10 @@ public class RangeTest {
 		rangeObject = null;
 	}
 
+	/*
+	 * GETCENTRALVALUE()
+	 */
+	
 	@Test
 	public void getCentralValueNominalTest() {
 		double centralResult = rangeObject.getCentralValue();
@@ -59,5 +63,98 @@ public class RangeTest {
 		rangeObject = new Range(minValDouble, maxValDouble);
 		assertEquals(middleValue, rangeObject.getCentralValue(), 0);
 	}
+	
+	/*
+	 * CONTAINS()
+	 */
+	
+	@Test
+	public void ContainsNominalTest(){
+		boolean isContained = rangeObject.contains(3);
+		assertEquals(true, isContained);
+	}
+	
+	@Test
+	public void DoesNotContainNominalTest(){
+		boolean isContained = rangeObject.contains(101);
+		assertEquals(false, isContained);
+	}
+	
+	@Test
+	public void ContainsMaximumTest(){
+		double maxValDouble = Double.MAX_VALUE;
+		rangeObject = new Range(0, maxValDouble);
+		assertEquals(true, rangeObject.contains(maxValDouble));
+	}
+	
+	@Test
+	public void DoesNotContainMaximumTest(){
+		double maxValDouble = Double.MAX_VALUE;
+		rangeObject = new Range(0, maxValDouble);
+		assertEquals(false, rangeObject.contains(-1));
+	}
+	
+	@Test
+	public void ContainsMinimumTest(){
+		double minValDouble = Double.MIN_VALUE;
+		rangeObject = new Range(minValDouble, 0);
+		assertEquals(true, rangeObject.contains(minValDouble));
+	}
+	
+	@Test
+	public void DoesNotContainMinimumTest(){
+		double minValDouble = Double.MIN_VALUE;
+		rangeObject = new Range(minValDouble, 0);
+		assertEquals(false, rangeObject.contains(-1));
+	}
+	
+	/*
+	 * EQUALS()
+	 */
 
+	@Test
+	public void EqualsNominalTest(){
+		Range rangeObjectTwo = new Range(-100, 100);
+		assertEquals(true, rangeObject.equals(rangeObjectTwo));
+	}
+	
+	@Test
+	public void DoesNotEqualNominalTestLower(){
+		Range rangeObjectTwo = new Range(-101, 100);
+		assertEquals(false, rangeObject.equals(rangeObjectTwo));
+	}
+	
+	@Test
+	public void DoesNotEqualNominalTestUpper(){
+		Range rangeObjectTwo = new Range(-100, 101);
+		assertEquals(false, rangeObject.equals(rangeObjectTwo));
+	}
+	
+	@Test
+	public void EqualsMaximumTest(){
+		double maxValDouble = Double.MAX_VALUE;
+		double minValDouble = Double.MIN_VALUE;
+		
+		rangeObject = new Range(minValDouble, maxValDouble);
+		Range rangeObjectTwo = new Range(minValDouble, maxValDouble);
+		assertEquals(true, rangeObject.equals(rangeObjectTwo));
+	}
+	
+	@Test
+	public void DoesNotEqualMaximumTest(){
+		double maxValDouble = Double.MAX_VALUE;
+		double minValDouble = Double.MIN_VALUE;
+		
+		Range rangeObjectTwo = new Range(minValDouble, maxValDouble);
+		assertEquals(false, rangeObject.equals(rangeObjectTwo));
+	}
+	
+	@Test
+	public void EqualsMinimumTest(){
+		
+		rangeObject = new Range(0, 0);
+		Range rangeObjectTwo = new Range(0, 0);
+		assertEquals(true, rangeObject.equals(rangeObjectTwo));
+	}
+	
 }
