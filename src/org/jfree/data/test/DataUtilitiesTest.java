b89b19a3d16966ee937c405efa07d5c18f6e7395
Gurvir Singh <gur_vir_30@hotmail.com>
2017-02-15 15:58:21 -0700

    Added tests for column total and allow for more values in Values2D

--- src/org/jfree/data/test/DataUtilitiesTest.java
+++ src/org/jfree/data/test/DataUtilitiesTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.jfree.data.Values2D;
 import org.jfree.data.DataUtilities;
-import org.jfree.data.KeyedValues;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.After;
@@ -26,34 +25,116 @@ public class DataUtilitiesTest {
 		mockingContext.checking(new Expectations() {
 			{
 				one(values).getRowCount();   
-				will(returnValue(2));      
+				will(returnValue(8));      
 
 				one(values).getValue(0, 0);     
 				will(returnValue(7.5));     
 				one(values).getValue(1, 0);  
-				will(returnValue(2.5));  
+				will(returnValue(2.5));
+				one(values).getValue(2, 0);     
+				will(returnValue(0));     
+				one(values).getValue(3, 0);  
+				will(returnValue(0)); 
+				one(values).getValue(4, 0);     
+				will(returnValue(0));     
+				one(values).getValue(5, 0);  
+				will(returnValue(0)); 
+				one(values).getValue(6, 0);     
+				will(returnValue(0));     
+				one(values).getValue(7, 0);  
+				will(returnValue(0)); 
 				
-				// Max Values
-				one(values).getValue(0,1);
+				/*******************/
+				/* *** COLUMNS *** */
+				/*******************/
+				
+				// Zero Test (Columns)
+				one(values).getValue(0, 2);
+				will(returnValue(0));
+				one(values).getValue(1, 2);
+				will(returnValue(0));
+				one(values).getValue(2, 2);     
+				will(returnValue(0));     
+				one(values).getValue(3, 2);  
+				will(returnValue(0)); 
+				one(values).getValue(4, 2);     
+				will(returnValue(0));     
+				one(values).getValue(5, 2);  
+				will(returnValue(0)); 
+				one(values).getValue(6, 2);     
+				will(returnValue(0));     
+				one(values).getValue(7, 2);  
+				will(returnValue(0)); 
+				
+				// Max Values Tests (Columns)
+				one(values).getValue(0, 3);
 				will(returnValue(maxValDouble));
-				one(values).getValue(1,1);
+				one(values).getValue(1, 3);
 				will(returnValue(0));
+				one(values).getValue(2, 3);     
+				will(returnValue(0));     
+				one(values).getValue(3, 3);  
+				will(returnValue(0)); 
+				one(values).getValue(4, 3);     
+				will(returnValue(0));     
+				one(values).getValue(5, 3);  
+				will(returnValue(0)); 
+				one(values).getValue(6, 3);     
+				will(returnValue(0));     
+				one(values).getValue(7, 3);  
+				will(returnValue(0)); 
 				
-				one(values).getValue(0,2);
+				one(values).getValue(0, 4);
 				will(returnValue(Math.floor(maxValDouble/2)));
-				one(values).getValue(1,2);
+				one(values).getValue(1, 4);
 				will(returnValue(Math.floor(maxValDouble/2)));
+				one(values).getValue(2, 4);     
+				will(returnValue(0));     
+				one(values).getValue(3, 4);  
+				will(returnValue(0)); 
+				one(values).getValue(4, 4);     
+				will(returnValue(0));     
+				one(values).getValue(5, 4);  
+				will(returnValue(0)); 
+				one(values).getValue(6, 4);     
+				will(returnValue(0));     
+				one(values).getValue(7, 4);  
+				will(returnValue(0)); 
 				
-				// Min Values
-				one(values).getValue(0,3);
+				// Min Values Tests (Columns)
+				one(values).getValue(0, 5);
 				will(returnValue(minValDouble));
-				one(values).getValue(1,3);
+				one(values).getValue(1, 5);
 				will(returnValue(0));
+				one(values).getValue(2, 5);     
+				will(returnValue(0));     
+				one(values).getValue(3, 5);  
+				will(returnValue(0)); 
+				one(values).getValue(4, 5);     
+				will(returnValue(0));     
+				one(values).getValue(5, 5);  
+				will(returnValue(0)); 
+				one(values).getValue(6, 5);     
+				will(returnValue(0));     
+				one(values).getValue(7, 5);  
+				will(returnValue(0)); 
 				
-				one(values).getValue(0,4);
+				one(values).getValue(0, 6);
 				will(returnValue(Math.ceil(minValDouble/2)));
-				one(values).getValue(1,4);
+				one(values).getValue(1, 6);
 				will(returnValue(Math.ceil(minValDouble/2)));
+				one(values).getValue(2, 6);     
+				will(returnValue(0));     
+				one(values).getValue(3, 6);  
+				will(returnValue(0)); 
+				one(values).getValue(4, 6);     
+				will(returnValue(0));     
+				one(values).getValue(5, 6);  
+				will(returnValue(0)); 
+				one(values).getValue(6, 6);     
+				will(returnValue(0));     
+				one(values).getValue(7, 6);  
+				will(returnValue(0)); 
 			}
 		});
 	}
@@ -72,6 +153,23 @@ public class DataUtilitiesTest {
 	public void calculateColumnTotalNominalTest() {
 		double result = DataUtilities.calculateColumnTotal(values, 0);
 		assertEquals(result, 10.0, .000000001d);
+	}
+	
+	@Test
+	public void calculateColumnTotalFalseNominalTest() {
+		try {
+			DataUtilities.calculateColumnTotal(values, 15);
+			fail();
+		}
+		catch(Error e){
+			assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void calculateColumnTotalZeroTest() {
+		double result = DataUtilities.calculateColumnTotal(values, 5);
+		assertEquals(result, 0, .000000001d);
 	}
 	
 	@Test
