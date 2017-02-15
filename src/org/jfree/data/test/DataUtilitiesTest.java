3cd77df5d82d1b24e03992144eff057d893113b2
Gurvir Singh <gur_vir_30@hotmail.com>
2017-02-15 15:16:32 -0700

    Added a few more tests to data utilities

--- src/org/jfree/data/test/DataUtilitiesTest.java
+++ src/org/jfree/data/test/DataUtilitiesTest.java
@@ -25,17 +25,35 @@ public class DataUtilitiesTest {
 		values = mockingContext.mock(Values2D.class);
 		mockingContext.checking(new Expectations() {
 			{
-				one(values).getRowCount();     
+				one(values).getRowCount();   
 				will(returnValue(2));      
+
 				one(values).getValue(0, 0);     
 				will(returnValue(7.5));     
 				one(values).getValue(1, 0);  
 				will(returnValue(2.5));  
 				
+				// Max Values
 				one(values).getValue(0,1);
 				will(returnValue(maxValDouble));
 				one(values).getValue(1,1);
 				will(returnValue(0));
+				
+				one(values).getValue(0,2);
+				will(returnValue(Math.floor(maxValDouble/2)));
+				one(values).getValue(1,2);
+				will(returnValue(Math.floor(maxValDouble/2)));
+				
+				// Min Values
+				one(values).getValue(0,3);
+				will(returnValue(minValDouble));
+				one(values).getValue(1,3);
+				will(returnValue(0));
+				
+				one(values).getValue(0,4);
+				will(returnValue(Math.ceil(minValDouble/2)));
+				one(values).getValue(1,4);
+				will(returnValue(Math.ceil(minValDouble/2)));
 			}
 		});
 	}
@@ -57,8 +75,26 @@ public class DataUtilitiesTest {
 	}
 	
 	@Test
-	public void calculateColumnTotalMaximumTest() {		
+	public void calculateColumnTotalMaximumTest1() {		
 		double result = DataUtilities.calculateColumnTotal(values, 1);
 		assertEquals(result, maxValDouble, .000000001d);
+	}
+	
+	@Test
+	public void calculateColumnTotalMaximumTest2() {
+		double result = DataUtilities.calculateColumnTotal(values, 2);
+		assertEquals(result, maxValDouble, .000000001d);
+	}
+	
+	@Test
+	public void calculateColumnTotalMinimumTest1() {		
+		double result = DataUtilities.calculateColumnTotal(values, 3);
+		assertEquals(result, minValDouble, .000000001d);
+	}
+	
+	@Test
+	public void calculateColumnTotalMinimumTest2() {
+		double result = DataUtilities.calculateColumnTotal(values, 4);
+		assertEquals(result, minValDouble, .000000001d);
 	}
 }
