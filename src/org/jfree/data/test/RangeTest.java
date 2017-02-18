d8ae594e0bfafdccb0feb449c9600b1ddd4d4f30
nbrons <nickbrons@msn.com>
2017-02-17 19:30:37 -0700

    added comments on Range functions

--- src/org/jfree/data/test/RangeTest.java
+++ src/org/jfree/data/test/RangeTest.java
@@ -29,12 +29,14 @@ public class RangeTest {
 	 * GETCENTRALVALUE()
 	 */
 	
+	//Nominal value for getCentralValue
 	@Test
 	public void getCentralValueNominalTest() {
 		double centralResult = rangeObject.getCentralValue();
 		assertEquals(0, centralResult, 0);		
 	}
 	
+	//Central value where a range difference is 0
 	@Test
 	public void getCentralValueNoRangeTest() {
 		rangeObject = new Range(-1,-1);
@@ -42,18 +44,21 @@ public class RangeTest {
 		assertEquals(-1, centralResult, 0);		
 	}
 	
+	//Central value for a range of 0 to the largest possible double value
 	@Test
 	public void getCentralValueMaximumTest() {
 		rangeObject = new Range(0, maxValDouble);
 		assertEquals(maxValDouble/2, rangeObject.getCentralValue(), 0);
 	}
 	
+	//Central value for a range of the smallest possible double to 0
 	@Test
 	public void getCentralValueMinimumTest() {
 		rangeObject = new Range(minValDouble, 0);
 		assertEquals(minValDouble/2, rangeObject.getCentralValue(), 0);
 	}
 	
+	//Central value for a range from the smallest to the largest possible double
 	@Test
 	public void getCentralValueMaximumRangeTest() {
 		double middleValue = (maxValDouble + minValDouble) / 2;
@@ -65,36 +70,42 @@ public class RangeTest {
 	 * CONTAINS()
 	 */
 	
+	//Checks if a value is contained in a nominal range
 	@Test
 	public void ContainsNominalTest(){
 		boolean isContained = rangeObject.contains(3);
 		assertEquals(true, isContained);
 	}
 	
+	//checks if a values is not contained in a nominal range
 	@Test
 	public void DoesNotContainNominalTest(){
 		boolean isContained = rangeObject.contains(101);
 		assertEquals(false, isContained);
 	}
 	
+	//checks if a value is contained from 0 to the maximum possible double
 	@Test
 	public void ContainsMaximumTest(){
 		rangeObject = new Range(0, maxValDouble);
 		assertEquals(true, rangeObject.contains(maxValDouble));
 	}
 	
+	//checks if a value is not contained in a range from 0 to the maximum possible double
 	@Test
 	public void DoesNotContainMaximumTest(){
 		rangeObject = new Range(0, maxValDouble);
 		assertEquals(false, rangeObject.contains(-1));
 	}
 	
+	//Contains for a range of the smallest possible double to 0
 	@Test
 	public void ContainsMinimumTest(){
 		rangeObject = new Range(minValDouble, 0);
 		assertEquals(true, rangeObject.contains(minValDouble));
 	}
 	
+	//Does not contain for a range of the smallest possible double to 0
 	@Test
 	public void DoesNotContainMinimumTest(){
 		rangeObject = new Range(minValDouble, 0);
@@ -105,24 +116,28 @@ public class RangeTest {
 	 * EQUALS()
 	 */
 
+	//Two nominal ranges are equal
 	@Test
 	public void EqualsNominalTest(){
 		Range rangeObjectTwo = new Range(-100, 100);
 		assertEquals(true, rangeObject.equals(rangeObjectTwo));
 	}
 	
+	//nominal ranges are not equal, differing on the lower end
 	@Test
 	public void DoesNotEqualNominalTestLower(){
 		Range rangeObjectTwo = new Range(-101, 100);
 		assertEquals(false, rangeObject.equals(rangeObjectTwo));
 	}
 	
+	//nominal ranges are equal differing on the upper end
 	@Test
 	public void DoesNotEqualNominalTestUpper(){
 		Range rangeObjectTwo = new Range(-100, 101);
 		assertEquals(false, rangeObject.equals(rangeObjectTwo));
 	}
 	
+	//Largest possible ranges are equal
 	@Test
 	public void EqualsMaximumTest(){
 		rangeObject = new Range(minValDouble, maxValDouble);
@@ -130,12 +145,14 @@ public class RangeTest {
 		assertEquals(true, rangeObject.equals(rangeObjectTwo));
 	}
 	
+	//Largest possible ranges are not equal
 	@Test
 	public void DoesNotEqualMaximumTest(){
 		Range rangeObjectTwo = new Range(minValDouble, maxValDouble);
 		assertEquals(false, rangeObject.equals(rangeObjectTwo));
 	}
 	
+	//smallest possible difference in ranges are equal
 	@Test
 	public void EqualsMinimumTest(){
 		rangeObject = new Range(0, 0);
@@ -147,27 +164,32 @@ public class RangeTest {
 	 * INTERSECTS()
 	 */
 	
+	//Two nominal ranges intersect
 	@Test
 	public void IntersectsNominalTrueTest(){
 		assertEquals(true, rangeObject.intersects(-100, 100));
 	}
 	
+	//Nominal ranges intersect on bottom
 	@Test
 	public void IntersectsBottomTest(){
 		assertEquals(true, rangeObject.intersects(-100, 0));
 	}
 	
+	//Nominal ranges intersect on top
 	@Test
 	public void IntersectsTopTest(){
 		assertEquals(true, rangeObject.intersects(0, 99));
 	}
 	
+	//Nominal ranges intersect with a difference of 0
 	@Test
 	public void IntersectsMinimumRangeTest(){
 		rangeObject = new Range(0,0);	
 		assertEquals(true, rangeObject.intersects(0, 0));
 	}
 	
+	//nominal ranges intersect of maximum possible size
 	@Test
 	public void IntersectsMaximumRangeTest(){
 		Range rangeObject = new Range(minValDouble, maxValDouble);
@@ -178,6 +200,7 @@ public class RangeTest {
 	 * SHIFT()
 	 */
 	
+	//Nominal shift test, shifting values by 5
 	@Test
 	public void ShiftNominalTest(){
 		double shiftValue = 5;
@@ -188,6 +211,7 @@ public class RangeTest {
 		assertEquals(true, rangeObjectTwo.equals(rangeObjectShifted));
 	}
 	
+	//Shifting by 0
 	@Test
 	public void ShiftMinimumShiftTest(){
 		double shiftValue = 0;
@@ -199,7 +223,7 @@ public class RangeTest {
 		assertEquals(true, rangeObjectTwo.equals(rangeObjectShifted));
 	}
 	
-	
+	//Shifting by maximum possible double
 	@Test
 	public void ShiftMaximumShiftTest(){
 		double shiftValue = maxValDouble;
@@ -212,6 +236,7 @@ public class RangeTest {
 		assertEquals(true, rangeObjectTwo.equals(rangeObjectShifted));
 	}
 	
+	//shift near maximum possible double
 	@Test
 	public void ShiftNearMaximumShiftTest(){
 		double shiftValue = maxValDouble -1;
@@ -224,6 +249,7 @@ public class RangeTest {
 		assertEquals(true, rangeObjectTwo.equals(rangeObjectShifted));
 	}
 	
+	//shift a negative (left) amount (by -1)
 	@Test
 	public void ShiftNegativeShiftTest(){
 		double shiftValue = -1;
